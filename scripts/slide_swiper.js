9d195d98f798577ee4c386854ef0fa0218d450dc
RichardViera <161188392+RichardViera@users.noreply.github.com>
2024-05-22 23:07:05 +1000

    Upload current website version

---
+++ scripts/slide_swiper.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('#cta-51 .swiper', {
+
+    speed: 1000,
+    spaceBetween: 0,
+    autoplay: true,
+    duration: 350,
+    loop: true,
+    fade: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
