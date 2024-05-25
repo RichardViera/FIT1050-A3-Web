c357d1351487696e760ee58d06aafc7d8f9fd832
RichardViera <161188392+RichardViera@users.noreply.github.com>
2024-05-26 01:02:19 +1000

    add a variety of products

--- scripts/product_collection.js
+++ scripts/product_collection.js
@@ -1,19 +1,11 @@
-$('.cs-button').click(filterItems);
-
-function filterItems() {
-    let button = this;
-    let selector = $(button).data('filter');
-    $('#collection-1602 .cs-listing .cs-item').not(selector).hide(500);
-    $(selector).show(500);
-}
-
 const product_swiper = new Swiper('#collection-1602 .swiper', {
 
     speed: 1100,
     slidesPerView: 4,
     spaceBetween: 1,
-    loop: true,
+    rewind: true,
     fade: true,
+    loopAdditionalSlides: 4,
 
     // Navigation arrows
     navigation: {
@@ -22,6 +14,14 @@ const product_swiper = new Swiper('#collection-1602 .swiper', {
     },
 });
 
+function filterItems() {
+    let button = this;
+    let selector = $(button).data('filter');
+    $('#collection-1602 .cs-listing').not(selector).hide(500);
+    $(selector).show(500);
+}
+
+$('#collection-1602 .cs-button').click(filterItems);
 $('#collection-1602 .swiper-wrapper').ready(hideButtons)
 $('#collection-1602 .swiper').hover(showButtons, hideButtons)
 
