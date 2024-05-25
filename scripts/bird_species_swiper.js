6d586973647e2b7aa67ff55bb99e16a95bd7394d
RichardViera <161188392+RichardViera@users.noreply.github.com>
2024-05-25 10:12:55 +1000

    update website

---
+++ scripts/bird_species_swiper.js
@@ -0,0 +1,14 @@
+const swiper2 = new Swiper('#collection-1577 .swiper', {
+
+    speed: 1100,
+    slidesPerView: 4,
+    spaceBetween: 0,
+    autoplay: true,
+    duration: 350,
+    loop: true,
+    fade: true,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    }
+});
